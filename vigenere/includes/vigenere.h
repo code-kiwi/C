1e0c8643df7a341cc7127b3773728166a1e3fea3
CodeKiwiFR <madric.hottinger.101@gmail.com>
2023-09-14 19:09:41 +0200

    Adds Makefile - Restructures project

---
+++ vigenere/includes/vigenere.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vigenere.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <mhotting@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 19:06:00 by mhotting          #+#    #+#             */
+/*   Updated: 2023/09/14 19:06:25 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VIGENERE_H
+# define VIGENERE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+#endif
